fdcbd6b13cc384bf02c1b2fb3b202cbd43017b44
RomanEnger <Roman.engertzisikov@gmail.com>
2024-04-27 14:37:40 +0300

    add week7 code

---
+++ Functions/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.WriteLine("Are you coming or leaving? (in/out):");
+string userChoice = Console.ReadLine();
+
+if (userChoice == "in")
+{
+    PrintHello();
+}
+else
+{
+    PrintGoodBye();
+}
+
+static void PrintHello() //function / method
+{
+    Console.WriteLine("Hello, World!");
+}
+
+static void PrintGoodBye()
+{
+    Console.WriteLine("See you later, aligator.");
+}
+
+7.hindeline test ulesanne 6 
